7e70c6315401d05af2e3ebbf87a6335abab42038
JesseSound <jesseearlemccormack@gmail.com>
2023-10-19 14:10:01 -0400

    made it so if it collides with player it does nothing

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -29,30 +29,37 @@ public class Collision : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-       
-        //create a for loop that will spawn particles around where the bullet collides before it is destroyed
-        //Use random numbers for x and Y position for a sense of fun
-        //change velocity of it for funnies using code we used for bullets as a template i think
-        //Particle prefab should have rigidbody so gravity makes it look silly
-        for (int i = 0; i < 10; i++)
+        if (collision.gameObject.name == "Player")
         {
-           GameObject sillyParticle = Instantiate(particlePrefab, transform.position + new Vector3(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f), 0.0f), transform.rotation);
-
-            
-
-            //adding velocity
-            sillyParticle.GetComponent<Rigidbody2D>().velocity = new Vector3(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 10.0f), 0.0f);
-
-            //pick random sprites everytime
-            int randomSpriteSelect = Random.Range(0, sprites.Count);
-            Sprite randomSpriteSelected = sprites[randomSpriteSelect];
-            sillyParticle.GetComponent<SpriteRenderer>().sprite = randomSpriteSelected;
-            //destroy the particle over time so it doesn't take up our valuable space!
-            Destroy( sillyParticle, Random.Range(1.0f,2.0f));
-            
+            Debug.Log(" no destroy plz");
         }
+        else
+        {
 
-        Destroy(gameObject);
+            Destroy(gameObject);
+
+            //create a for loop that will spawn particles around where the bullet collides before it is destroyed
+            //Use random numbers for x and Y position for a sense of fun
+            //change velocity of it for funnies using code we used for bullets as a template i think
+            //Particle prefab should have rigidbody so gravity makes it look silly
+            for (int i = 0; i < 10; i++)
+            {
+                GameObject sillyParticle = Instantiate(particlePrefab, transform.position + new Vector3(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f), 0.0f), transform.rotation);
+
+
+
+                //adding velocity
+                sillyParticle.GetComponent<Rigidbody2D>().velocity = new Vector3(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 10.0f), 0.0f);
+
+                //pick random sprites everytime by creating a list (done above) and picking a random number every loop
+                int randomSpriteSelect = Random.Range(0, sprites.Count);
+                Sprite randomSpriteSelected = sprites[randomSpriteSelect];
+                sillyParticle.GetComponent<SpriteRenderer>().sprite = randomSpriteSelected;
+                //destroy the particle over time so it doesn't take up our valuable space!
+                Destroy(sillyParticle, Random.Range(1.0f, 2.0f));
+
+            }
+
+        }
     }
 }
